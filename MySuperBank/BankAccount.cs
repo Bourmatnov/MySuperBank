d0efd326f96e860c8eaea79b8277fe0858283e7d
Paul Bourmatnov <paul.m.bourmatnov@boeing.com>
2020-06-18 13:45:37 -0700

    test2

--- MySuperBank/BankAccount.cs
+++ MySuperBank/BankAccount.cs
@@ -64,7 +64,6 @@ namespace MySuperBank
                 histreport.AppendLine($"{item.Date.ToShortDateString()}\t\t${item.Amount}\t {item.Notes}");
             }
             return histreport.ToString();
-            //New
         }
 
     }

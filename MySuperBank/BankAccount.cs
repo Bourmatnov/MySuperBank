e1f4d592245eeb248386a062ddb3c8c35da61e52
Paul Bourmatnov <paul.m.bourmatnov@boeing.com>
2020-06-18 12:22:53 -0700

    Add project files.

---
+++ MySuperBank/BankAccount.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; }
+        public decimal Balance {
+            get {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+        private static int accountseed = 12175;
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name,decimal initialBalance)
+        {
+            Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Deposit");
+            Number = accountseed.ToString();
+            accountseed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount must be posititve");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <=0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount withdrawn must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Insufficient Funds for Withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public string GetAccountHist()
+        {
+            var histreport = new StringBuilder();
+            histreport.AppendLine("Date\t\t\tAmount\tNote");
+            histreport.AppendLine();
+            foreach (var item in allTransactions)
+            {
+                histreport.AppendLine($"{item.Date.ToShortDateString()}\t\t${item.Amount}\t {item.Notes}");
+            }
+            return histreport.ToString();
+            //End of main code
+
+        }
+
+    }
+}
---
+++ MySuperBank/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Initial account create
+                var account = new BankAccount("Kendra", 10000);
+                Console.WriteLine($"Account {account.Number} was created for {account.Owner} with Balance of {account.Balance}!!!");
+                //Console.WriteLine($"Current DateTime is {DateTime.Now}");
+
+                // Make a valid withdrawal
+                account.MakeWithdrawal(120, Convert.ToDateTime("6/15/2020 8:51:05 AM"), "Sleeping Bag");
+                //Console.WriteLine(account.Balance);
+
+                // try to break by sending in negative deposit
+                //account.MakeDeposit(-200, DateTime.Now, "Steal");
+
+                // try to withdraw negative
+                //account.MakeWithdrawal(-200, DateTime.Now, "negative amount withdrawal");
+
+                // try to overdraw
+                //account.MakeWithdrawal(3445200, DateTime.Now, "New House");
+
+                // Deposit
+                account.MakeDeposit(450, DateTime.Now, "Extra Cash");
+
+                // Acceptable withdrawal
+                account.MakeWithdrawal(35, DateTime.Now, "Need cash");
+
+                Console.WriteLine(account.GetAccountHist());
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception: Negative deposit!");
+                Console.WriteLine(e.ToString());
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception: You cannot overdraw this account!");
+                Console.WriteLine(e.ToString());
+            }
+            catch
+            {
+                Console.WriteLine("Unknown Exception caught!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
